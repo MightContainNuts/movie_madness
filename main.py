a975cce040dec381cf021265d663bef66ee3ae79
MightContainNuts <mightcontainnuts@icloud.com>
2025-01-08 21:03:15 +0100

    bonus step finished

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
+import sys
+
 from movie_app import MovieApp
 from storage import StorageJson, StorageCsv
 from pathlib import Path
 from typing import Tuple
+from logger import setup_logger
 
-storage_dir = Path(__file__).resolve().parent / "data"
-storage_dir.mkdir(parents=True, exist_ok=True)
-
-storage_json = storage_dir / "movies.json"
-storage_csv = storage_dir / "movies.csv"
+logger = setup_logger(__name__)
 
 
-def get_start_params(params: str) -> Tuple[str, str, str]:
+def get_start_params(params: str) -> Tuple[str, str]:
     """
     validate args and return correctly
     :param arg_1:
@@ -20,12 +19,13 @@ def get_start_params(params: str) -> Tuple[str, str, str]:
     :return:
     :rtype:
     """
-    app_name, storage_name = params.lower().split(".")
-    app_name = app_name.strip()
-    storage_name = storage_name.strip()
-    if _is_valid_storage_type(storage_name):
-        storage_class = _return_storage_type(storage_name)
-        return params, app_name, storage_class
+
+    _, storage_name = params.lower().split(".")
+    storage_type = storage_name.strip()
+    if _is_valid_storage_type(storage_type):
+        storage_type = _return_storage_type(storage_name)
+        logger.info("Params parsed", storage_name, storage_type)
+        return params, storage_type
 
 
 def _is_valid_storage_type(storage_type: str) -> bool:
@@ -52,14 +52,28 @@ def _return_storage_type(storage_name: str) -> str:
     return storage_mapping[storage_name]
 
 
-def main():
-    with StorageCsv(storage_csv) as storage:
-        app1 = MovieApp(storage)
-        app1.run()
-    with StorageJson(storage_json) as storage:
-        app2 = MovieApp(storage)
-        app2.run()
+def main(storage_file, storage_type):
+
+    logger.info("New instance started %s, %s", storage_file, storage_type)
+    with StorageCsv(storage_file) as storage:
+        app = MovieApp(storage)
+        logger.info("New instance started %s, %s", storage_file, storage_type)
+        app.run()
 
 
 if __name__ == "__main__":
-    main()
+    storage_dir = Path(__file__).resolve().parent / "data"
+    storage_dir.mkdir(parents=True, exist_ok=True)
+
+    storage_json = storage_dir / "default.json"
+    storage_csv = storage_dir / "default.csv"
+
+    if len(sys.argv) == 2:
+        params = sys.argv[1]
+        storage_file, storage_class = get_start_params(params)
+        if storage_file and storage_class:
+            storage_path = storage_dir / storage_file
+    else:
+        storage_path = storage_json
+        storage_class = StorageJson
+    main(storage_path, storage_class)
